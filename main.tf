4f71b3759d5d83eabe2e39dc1eae590478230aec
Julien Céré Imboden <julien.cere.imboden@konverto.eu>
2021-02-04 10:09:43 +0100

    remove app service

--- main.tf
+++ main.tf
@@ -139,36 +139,3 @@ data "azurerm_public_ip" "ip" {
 output "public_ip_address" {
   value = data.azurerm_public_ip.ip.ip_address
 }
-
-resource "azurerm_app_service_plan" "aasp-two" {
-  name                = "example-appserviceplan-two"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_app_service" "aas-two" {
-  name                = "example-app-service-two"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.aasp-two.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  } 
-}
